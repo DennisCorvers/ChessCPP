61fd86c22b02c5480b942e3068ba46454f8b5b80
Dennis <denniscorvers@hotmail.com>
2020-05-24 05:50:37 +0200

    Basic project setup, Vsync/Fps counter

--- Chess/Main.cpp
+++ Chess/Main.cpp
@@ -1,1 +1,66 @@
-//https://samulinatri.com/blog/sfml-visual-studio-setup-cpp-tutorial/
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <Windows.h>
+
+#include "Utilities.h"
+
+sf::Font font;
+
+std::string floatToString(float number, int precision) {
+	std::stringstream stream;
+	stream << std::fixed << std::setprecision(precision) << number;
+	return stream.str();
+}
+
+sf::Text createText(sf::String& string, unsigned int fontSize) {
+	sf::Text text;
+	text.setFont(font);
+	text.setString(string);
+	text.setFillColor(sf::Color::White);
+	text.setCharacterSize(fontSize);
+
+	return text;
+}
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	window.setVerticalSyncEnabled(true);
+	
+	//if (!font.loadFromFile(Utils::GetWorkingDirectory("Assets\\Fonts\\OpenSans-Regular.ttf"))) {
+		if (!font.loadFromFile("Assets\\Fonts\\OpenSans-Regular.ttf")) {
+		std::cout << "Unable to load font file" << std::endl;
+		system("pause");
+	}
+
+	sf::Clock clock;
+	float lastTime = 0;
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		float currentTime = clock.restart().asSeconds();
+		float fps = 1.0f / currentTime;
+		lastTime = currentTime;
+
+		sf::String str(floatToString(fps, 1));
+		sf::Text text = createText(str, 18);
+
+		window.clear();
+		window.draw(text);
+		window.display();
+	}
+
+	return 0;
+}
+
---
+++ Chess/Utilities.h
@@ -0,0 +1,41 @@
+#pragma once
+#define RUNNING_WINDOWS
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+namespace Utils {
+#ifdef RUNNING_WINDOWS
+
+#include <windows.h>
+#include <Shlwapi.h>
+	inline std::string GetWorkingDirectory() {
+		HMODULE hModule = GetModuleHandle(nullptr);
+		if (hModule) {
+			char path[256];
+			GetModuleFileName(hModule, path, sizeof(path));
+			PathRemoveFileSpec(path);
+			strcat_s(path, "\\");
+			return std::string(path);
+		}
+		return "";
+	}
+
+	inline std::string GetWorkingDirectory(std::string filename) {
+		return GetWorkingDirectory() + filename;
+	}
+#elif defined RUNNING_LINUX
+#include <unistd.h>
+	inline std::string GetWorkingDirectory() {
+		char cwd[1024];
+		if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+			return std::string(cwd) + std::string("/");
+		}
+		return "";
+	}
+
+	inline std::string GetWorkingDirectory(std::string filename) {
+		return GetWorkingDirectory() + filename;
+	}
+#endif
+}
